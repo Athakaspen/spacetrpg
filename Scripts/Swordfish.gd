c9bee50f0fb3e7936d9963359611cece25f23da5
Athakaspen <35859545+Athakaspen@users.noreply.github.com>
2019-09-03 20:35:19 -0500

    Animation finished; html export

--- Scripts/Swordfish.gd
+++ Scripts/Swordfish.gd
@@ -1,22 +1,32 @@
 extends Node2D
 
-enum MODE {
+enum STATE {
 	WAITING_FOR_INPUT
-	ACCELERATING
-	DECELERATING
+	MOVING
+	}
+enum TURNSTATE {
+	STATIC
+	TURNING
+	}
+enum MOVESTATE {
+	STATIC
+	MOVING
 	}
 
 
-export var moveDist = 400;
+export var moveDist = 3000;
 
-var STATE;
+var acceleration = 500; # units/sec/sec
+var maxSpeed = 1000; # units/sec
+var angleAcceleration = 6; # rad/sec/sec
+var angleMaxspeed = 10; # rad/sec
+
+var State;
+var MoveState;
+var TurnState;
 var goalPosition;
-var acceleration = 500;
-var maxSpeed = 1500;
-var velocity = Vector2(0,0);
-var angle_acceleration = 1;
-var angle_maxspeed = 1;
-var angle_velocity = 0;
+var velocity = Vector2(0,0); # current velocity vector
+var turnVelocity = 0.0; # current turning speed (in radians)
 
 var draw_moveline;
 var draw_range;
@@ -24,44 +34,76 @@ var draw_range;
 var mousePos
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.STATE = MODE.WAITING_FOR_INPUT
+	self.State = STATE.WAITING_FOR_INPUT;
+	self.MoveState = MOVESTATE.STATIC;
+	self.TurnState = TURNSTATE.STATIC;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	# Determine what type of action to take
-	match self.STATE:
+	match self.State:
 		# Waiting for a new mouse click from user.
-		MODE.WAITING_FOR_INPUT:
+		STATE.WAITING_FOR_INPUT:
 			mousePos = get_viewport().get_mouse_position()
 			# Draw line from ship to mouse if mouse is in range.
 			draw_moveline = self.position.distance_to(mousePos) <= moveDist
 			draw_range = self.position.distance_to(mousePos) >= moveDist
 			if Input.is_action_just_pressed("MoveShip"):
-				if self.position.distance_to(mousePos) <= moveDist:
+				if self.position.distance_to(mousePos) <= moveDist and self.position.distance_to(mousePos) > 5: # Move if the new position is in range, but restrict exceptionally small movements.
 					initiateMove(mousePos)
-		
+
+				# OLD MOVEMENT CODE
 		# Animating movement to new location, do not accept new movement commands.
-		MODE.ACCELERATING:
-			self.velocity.y -= acceleration*delta; # add acceleration to velocity vector (-y = forward)
-			velocity.y = clamp(velocity.y, -maxSpeed, maxSpeed)
-			applyVelocity(delta)
-			if self.position.distance_to(goalPosition)<=pow(velocity.y,2)/(2*acceleration): # If the remaining distance is less than the braking distance,
-				self.STATE = MODE.DECELERATING; # begin decelerating.
-		
-		MODE.DECELERATING:
-			self.velocity.y += acceleration*delta; # subtract acceleration from velocity vector (+y = reverse)
-			applyVelocity(delta)
-			if velocity.y>=0: # if the ship has come to a stop,
-				velocity.y = 0; # remove all extra velocity
-				self.position = goalPosition; # move to exactly the target position
-				STATE = MODE.WAITING_FOR_INPUT # change state
+		STATE.MOVING:
+
+			match self.TurnState:
+				TURNSTATE.STATIC:
+					pass
+				TURNSTATE.TURNING:
+					var angleTo = self.get_angle_to(goalPosition)+PI/2; # get relative angle to goal position
+					if angleTo >= PI: angleTo = -2*PI+angleTo; #correct for ship direction not being 0 radians
+					if abs(angleTo)>pow(turnVelocity,2)/(2*angleAcceleration): # If the remaining angle is more than the braking angle,
+						turnVelocity += sign(angleTo)*angleAcceleration*delta # speed up
+						turnVelocity = clamp(turnVelocity, -angleMaxspeed, angleMaxspeed) # do not exceed max angular velocity
+					else: # If the remaining angle is less than or equal to the braking angle,
+						turnVelocity -= sign(angleTo)*angleAcceleration*delta # slow down
+					applyAngularVelocity(delta);
+					angleTo = self.get_angle_to(goalPosition)+PI/2; # get relative angle to goal position
+					if angleTo >= PI: angleTo = -2*PI+angleTo; #correct for ship direction not being 0 radians
+					if MoveState == MOVESTATE.STATIC and abs(angleTo) <= clamp(self.position.distance_to(goalPosition)/(acceleration/4),0,1)*PI/2:
+						MoveState = MOVESTATE.MOVING
+					if abs(angleTo) <= 0.02: # snap to angle if within given range
+						self.rotation += angleTo
+						turnVelocity = 0.0
+						self.TurnState = TURNSTATE.STATIC
+
+			match self.MoveState:
+				MOVESTATE.STATIC:
+					pass
+				MOVESTATE.MOVING:
+					var distanceTo = self.position.distance_to(goalPosition)
+					if distanceTo>pow(velocity.y,2)/(2*acceleration): # If the remaining distance is greater than the braking distance,
+						self.velocity.y -= acceleration*delta; # add acceleration to velocity vector (-y = forward)
+						velocity.y = clamp(velocity.y, -maxSpeed, maxSpeed)
+					else:
+						self.velocity.y += acceleration*delta; # subtract acceleration from velocity vector (+y = reverse)
+					applyVelocity(delta)
+					if self.position.distance_to(goalPosition) >= distanceTo:
+						velocity.y = 0; # remove all extra velocity
+						self.position = goalPosition; # move to exactly the goal position
+						MoveState = MOVESTATE.STATIC;
+						TurnState = TURNSTATE.STATIC;
+						turnVelocity = 0.0
+						State = STATE.WAITING_FOR_INPUT;
+
 
 	# Run the draw function each frame
 	update()
 
 func _draw():
 	if draw_range:
-		draw_ring(Vector2(0,0), moveDist, 2, Color.white, 64)
+		draw_ring(Vector2(0,0), moveDist, 2, Color.white, 84)
 	if draw_moveline == true:
 		draw_line(Vector2(0,0), to_local(mousePos), Color.red, 2.5, true)
 
@@ -73,14 +115,19 @@ func draw_ring(center:Vector2, radius:float, thickness:float, color:Color, resol
 	
 	while drawCounter<resolution:
 		endl = startl.rotated(2*PI/resolution)
-		draw_line(startl, endl, color, thickness, true)
+		if !drawCounter%3:
+			draw_line(startl, endl, color, thickness, true)
 		startl = endl
 		drawCounter+=1
 
 func initiateMove(Position):
 	self.goalPosition = Position
-	self.rotation += get_angle_to(goalPosition)+deg2rad(90) # instantly rotate to face target
-	self.STATE = MODE.ACCELERATING
+	self.State = STATE.MOVING
+	self.TurnState = TURNSTATE.TURNING
+	self.MoveState = MOVESTATE.STATIC
 
 func applyVelocity(delta):
 	self.position += velocity.rotated(self.rotation)*delta # Apply velocity relative to ship OCS
+
+func applyAngularVelocity(delta):
+	self.rotation += turnVelocity*delta # apply angular velocity to currect rotation
